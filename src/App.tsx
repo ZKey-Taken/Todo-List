73b21502ca7ce3f9ab1f32c3b96ef309476564a6
Z_Key <83915288+ZKey-Taken@users.noreply.github.com>
2024-05-26 02:51:25 -0400

    TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [task, setTask] = useState<string>("");
+  const [todoList, setTodoList] = useState<string[]>([]);
+  
+  function handleChange(event: ChangeEvent<HTMLInputElement>): void{
+    event.preventDefault();
+    setTask(event.target.value);
+  };
+
+  function addTask(): void{
+    if(task.length > 0){ // Add only if task isn't empty
+      setTodoList([...todoList, task]); // Adds task into todo list
+    }
+  };
+
+  function TodoTaskList(task: string){
+      return (
+        <div className='tasks'>
+          <div>{task}</div>
+          <button>Remove</button>
+        </div>
+      )
+  };
+
+  function removeTask(taskToDelete: string): void{
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todolist-app'>
+      <div className='header'>
+        <input type='text' placeholder='Enter A Task' onChange={handleChange}/>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className='todolist'>
+        {todoList.map(TodoTaskList)}
+      </div>
     </div>
   );
 }
